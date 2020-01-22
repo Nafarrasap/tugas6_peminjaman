f2b029e3c378da29d96c2d373693b23c3bf813da
nafarrasap <nafarras_ayu_27rpl@student.smktelkom-mlg.sch.id>
2020-01-22 21:51:46 +0700

    first commit

--- app/PetugasModel.php
+++ app/PetugasModel.php
@@ -7,18 +7,18 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class PetugasModel extends Authenticatable implements JWTSubject
 {
     use Notifiable;
+    protected $table = 'petugas';
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-     protected $table = 'petugas';
     protected $fillable = [
-        'nama_petugas', 'alamat','telp','username', 'password','level',
+        'nama_petugas', 'alamat','telp', 'username', 'password', 'level',
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,17 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+
 });
+// Route::post('register', 'UserController@register');
+// Route::post('login', 'UserController@login');
+// Route::get('book', 'BookController@book');
+//
+// Route::get('bookall', 'BookController@bookAuth')->middleware('jwt.verify');
+Route::get('user', 'UserController@getAuthenticatedUser')->middleware('jwt.verify');
+Route::get('/',function(){
+  return Auth::user()->alamat;
+})->middleware('jwt.verify');
+
+Route::post('register', 'PetugasController@register');
+Route::post('login', 'PetugasController@login');
